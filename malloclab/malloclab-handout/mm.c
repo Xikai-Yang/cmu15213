c82915439da10b248e439769c2c29f16e190d73f
Xikai-Yang <18810921006@163.com>
2020-08-02 16:20:33 +0800

    explicit version

--- malloclab/malloclab-handout/mm.c
+++ malloclab/malloclab-handout/mm.c
@@ -58,6 +58,14 @@ team_t team = {
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char *)(bp) - (SIZE_T_SIZE*2)))
 #define PUT(p, val) ((*(size_t *)(p)) = (val))
 #define FACTOR 2
+#define MINIMUM_SIZE ((SIZE_T_SIZE)*4)
+
+#define PUT_PTR(p, ptr) ((*(void **)(p)) = ptr)
+#define GET_PTR(bp) (*(void **)(bp))
+#define GET_PREV(bp) GET_PTR((bp))
+#define GET_NEXT(bp) GET_PTR((bp + SIZE_T_SIZE))
+#define MAX(a, b) ((a > b)? (a) : (b))
+
 
 
 /* 
@@ -67,18 +75,19 @@ int mm_init(void)
 {
 
     //virtual_end = mem_heap_hi();
-    int total_size = SIZE_T_SIZE * 4;
+    int total_size = SIZE_T_SIZE * 6;
     void *heap_start = mem_sbrk(total_size);
-    PUT(heap_start, PACK(16, 1));
-    PUT((heap_start + SIZE_T_SIZE), PACK(16, 1));
-    PUT((heap_start + SIZE_T_SIZE * 2), PACK(16, 1));
-    PUT((heap_start + SIZE_T_SIZE * 3), PACK(16, 1));
 
+    PUT(heap_start, PACK(SIZE_T_SIZE * 4, 1));
+    PUT_PTR(heap_start + SIZE_T_SIZE, heap_start + SIZE_T_SIZE);
+    PUT_PTR(heap_start + SIZE_T_SIZE * 2, heap_start + SIZE_T_SIZE);
+    PUT((heap_start + SIZE_T_SIZE * 3), PACK(SIZE_T_SIZE * 4, 1));
+
+    PUT((heap_start + SIZE_T_SIZE * 4), PACK(SIZE_T_SIZE * 2, 1));
+    PUT((heap_start + SIZE_T_SIZE * 5), PACK(SIZE_T_SIZE * 2, 1));
 
     return 0;
 }
-
-
 void print_heap() {
     char * ptr = mem_heap_lo();
     void *physical_boundary = mem_heap_hi() + 1 - SIZE_T_SIZE * 2;
@@ -97,7 +106,89 @@ void print_heap() {
     printf("====================\n");
 
 }
-void coalesce(void *ptr);
+
+void append_list(void *bp)
+{
+    void *sentinel = mem_heap_lo() + SIZE_T_SIZE;
+    void *original_first = GET_PTR(sentinel + SIZE_T_SIZE);
+    PUT_PTR(sentinel + SIZE_T_SIZE, bp);  
+    PUT_PTR(original_first, bp);
+    PUT_PTR(bp, sentinel);
+    PUT_PTR(bp + SIZE_T_SIZE, original_first);
+}
+
+void delete_list(void *bp)
+{
+    /*
+    * a free block is allocated
+    * and should be deleted from the free list
+    */
+   void *prev_node = GET_PREV(bp);
+   void *next_node = GET_NEXT(bp);
+   PUT_PTR((prev_node + SIZE_T_SIZE), next_node);
+   PUT_PTR(next_node, prev_node);
+}
+
+void *extend_heap(size_t alloc_size);
+
+void *first_fit(size_t alloc_size)
+{ 
+    /*
+    * return header pointer instead of base pointer
+    */
+    
+    void *list_start = mem_heap_lo() + SIZE_T_SIZE;
+    void *temp_ptr = GET_NEXT(list_start);
+
+    while (temp_ptr != list_start) {
+        int size = GET_SIZE(HDRP(temp_ptr));
+        int alloc = GET_ALLOC(HDRP(temp_ptr));
+        if (alloc == 0 && size >= alloc_size) {
+            delete_list(temp_ptr);
+            return (temp_ptr - SIZE_T_SIZE);
+        }
+        temp_ptr = GET_NEXT(temp_ptr);
+    }
+    if (temp_ptr == list_start) {
+        void *ptr = extend_heap(alloc_size * FACTOR);
+        return ptr;
+    }
+    return NULL;
+}
+
+void *best_fit(size_t alloc_size)
+{
+    void *list_start = mem_heap_lo() + SIZE_T_SIZE;
+    void *temp_ptr = GET_NEXT(list_start);
+    size_t min_size = 0x7fffffff;
+    void *min_ptr = NULL;
+    while (temp_ptr != list_start) {
+        int size = GET_SIZE(HDRP(temp_ptr));
+        
+        if (size == alloc_size) {
+            delete_list(temp_ptr);
+            return (temp_ptr - SIZE_T_SIZE);
+        }
+        if (size > alloc_size) {
+            if (size < min_size) {
+                min_size = size;
+                min_ptr = temp_ptr;
+            }
+        }
+        temp_ptr = GET_NEXT(temp_ptr);
+    }
+    if (min_ptr == NULL) {
+        void *ptr = extend_heap(alloc_size * FACTOR);
+        return ptr;
+    } else {
+        delete_list(min_ptr);
+        return (min_ptr - SIZE_T_SIZE);
+    }
+    return NULL;
+}
+
+
+void *coalesce(void *ptr);
 void *extend_heap(size_t alloc_size)
 {
     void *old_brk = mem_sbrk(alloc_size);
@@ -112,116 +203,33 @@ void *extend_heap(size_t alloc_size)
     PUT(footer + SIZE_T_SIZE, PACK(16,1));
     PUT(footer + SIZE_T_SIZE * 2, PACK(16, 1));
 
-    coalesce(old_brk + SIZE_T_SIZE);
-    return old_brk;
-}
-void *best_fit(size_t alloc_size)
-{
-    char * ptr = mem_heap_lo() + SIZE_T_SIZE * 2;
-    void *physical_boundary = mem_heap_hi() + 1 - SIZE_T_SIZE * 2; // (brk - 1) + 1
-    int delta = (char *)(physical_boundary) - ptr;
-    if (delta == 0) {
-        void * old_brk = extend_heap(alloc_size * FACTOR);
-        return old_brk;
-    }
-    size_t min_size = 0x7fffffff;
-    void * min_ptr = NULL;
-    while (1) {
-        int size = GET_SIZE(ptr);
-        int alloc = GET_ALLOC(ptr);
-        if (alloc == 1) {
-            if (ptr == physical_boundary) {
-                if (min_ptr != NULL) {
-                    return min_ptr;
-                } else {
-                    extend_heap(alloc_size * FACTOR);
-                    return ptr;
-                }
-            }
-
-            ptr += size;
-            continue;
-        }
-
-        if (alloc == 0) {
-            if (size == alloc_size) {
-                return (void *)ptr;
-            }
-            if (size > alloc_size) {
-                if (min_size > size) {
-                    min_ptr = ptr;
-                    min_size = size;
-                }
-                ptr += size;
-            }
-            else {
-                void *footer = ptr + GET_SIZE(ptr) - SIZE_T_SIZE;
-                if (footer + SIZE_T_SIZE == physical_boundary) {
-                    
-                    if (min_ptr != NULL) {
-                        return min_ptr;
-                    }
-                    
-                    int delta_size = (ptr + alloc_size) - (char *)(physical_boundary);
-                    extend_heap(delta_size * FACTOR);
-                    return ptr;
-                }
-                ptr += size;
-
-            }
-        }
-
-    }
+    void *returned_ptr = coalesce(old_brk + SIZE_T_SIZE);
+    return returned_ptr;
 }
 
-void *first_fit(size_t alloc_size)
+void print_list()
 {
-    
-    char * ptr = mem_heap_lo() + SIZE_T_SIZE * 2;
-    void *physical_boundary = mem_heap_hi() + 1 - SIZE_T_SIZE * 2; // (brk - 1) + 1
-    int delta = (char *)(physical_boundary) - ptr;
-    if (delta == 0) {
-        void * old_brk = extend_heap(alloc_size * FACTOR);
-        return old_brk;
+    void *list_start = mem_heap_lo() + SIZE_T_SIZE;
+    void *temp_ptr = GET_NEXT(list_start);
+    printf("node address is %p, size is %d and alloc is %d\n",list_start-SIZE_T_SIZE,32,1);
+    while (temp_ptr != list_start) {
+        int size = GET_SIZE(HDRP(temp_ptr));
+        int alloc = GET_ALLOC(HDRP(temp_ptr));
+        printf("node address is %p, size is %d and alloc is %d\n", 
+        (temp_ptr - SIZE_T_SIZE), size, alloc);
+        temp_ptr = GET_NEXT(temp_ptr);
     }
-    while (1)
-    {
-        // if delta > 0
-        int size = GET_SIZE(ptr);
-        int alloc = GET_ALLOC(ptr);
-        if (alloc == 1) {
-            if (ptr == physical_boundary) {
-                // prevent ptr is the last sentinel node
-                extend_heap(alloc_size * FACTOR);
-                return ptr;
-            }
-            ptr += size;
-            continue;
-        }
-        if (alloc == 0) {
-            if (size >= alloc_size) {
-                return (void *) ptr;
-            } else {
-                void *footer = ptr + GET_SIZE(ptr) - SIZE_T_SIZE;
-                if (footer + SIZE_T_SIZE == physical_boundary) {
-                    // last node
-                    int delta_size = (ptr + alloc_size) - (char *)(physical_boundary);
-                    extend_heap(delta_size * FACTOR);
-                    return ptr;
-                }
-                //printf("ptr is %p and size is %d\n", ptr, size);
-                ptr += size;
-            }
-        }
-        
-    }
-    
+
 }
 
 void *find_fit(size_t alloc_size) 
 {
-    void * ptr = first_fit(alloc_size);
-    //void * ptr = best_fit(alloc_size);
+    /*
+    * input is allocation size
+    * output is header pointer instead of base pointer
+    */
+    //void * ptr = first_fit(alloc_size);
+    void *ptr = best_fit(alloc_size);
     if (ptr == NULL) {
         printf("stack overflow\n");
         exit(1);
@@ -240,27 +248,40 @@ void *mm_malloc(size_t size)
         return NULL;
     }
 
-    size_t newsize = ALIGN(size + SIZE_T_SIZE * 2);
+    size_t newsize = MAX(ALIGN(size + SIZE_T_SIZE * 2), MINIMUM_SIZE);
     void *ptr = find_fit(newsize);
+    delete_list(ptr + SIZE_T_SIZE);
     size_t oldsize = GET_SIZE(ptr);
     int delta_size = oldsize - newsize;
+    if (delta_size < MINIMUM_SIZE) {
+        newsize = oldsize;
+    }
     PUT(ptr, PACK(newsize, 1));
     void *footer = (void *)((char *)ptr + newsize - SIZE_T_SIZE); // void ptr arithmetic undefined
     PUT(footer, PACK(newsize, 1));
     
-    if (delta_size > 0) {
+    if (delta_size >= MINIMUM_SIZE) {
+        // pay attention to the equal sign or it will cause bugs!
         // splitting!
         PUT(footer + SIZE_T_SIZE, PACK(delta_size, 0));
         PUT(footer + delta_size, PACK(delta_size, 0));
+        append_list(footer + SIZE_T_SIZE * 2);
     }
-    
-
+    //printf("malloc : %d\n", size);
+    /*
+    printf("malloc : %d\n", size);
+    print_list();
+    print_heap();
+    */
     return (void *) ((char *)ptr + SIZE_T_SIZE);
 }
 
-void coalesce(void *ptr)
+void *coalesce(void *ptr)
 {
     /*
+    * intput is base pointer
+    * output is header pointer pointing to the start of that region
+    * 
     * if we didn't introduce sentinel node
     * then there will be many corner cases
     * e.g.
@@ -282,6 +303,8 @@ void coalesce(void *ptr)
     if (prev_alloc && next_alloc) {
         PUT(header, PACK(size, 0));
         PUT(footer, PACK(size, 0));
+        append_list(ptr);
+        return header;
     }
 
     if (prev_alloc && !next_alloc) {
@@ -289,6 +312,9 @@ void coalesce(void *ptr)
         int new_size = size + GET_SIZE(next_header);
         PUT(header, PACK(new_size, 0));
         PUT(next_footer, PACK(new_size, 0));
+        delete_list(next_header + SIZE_T_SIZE);
+        append_list(ptr);
+        return header;
     }
 
     if (!prev_alloc && next_alloc) {
@@ -296,6 +322,9 @@ void coalesce(void *ptr)
         int new_size = size + GET_SIZE(prev_footer);
         PUT(footer, PACK(new_size, 0));
         PUT(prev_header, PACK(new_size, 0));
+        delete_list(prev_header + SIZE_T_SIZE);
+        append_list(prev_header + SIZE_T_SIZE);
+        return prev_header;
     }
 
     if (!prev_alloc && !next_alloc) {
@@ -304,8 +333,12 @@ void coalesce(void *ptr)
         int new_size = size + GET_SIZE(next_header) + GET_SIZE(prev_footer);
         PUT(prev_header, PACK(new_size, 0));
         PUT(next_footer, PACK(new_size, 0));
+        delete_list(prev_header + SIZE_T_SIZE);
+        delete_list(next_header + SIZE_T_SIZE);
+        append_list(prev_header + SIZE_T_SIZE);
+        return prev_header;
     }
-
+    return NULL;
     
 }
 
@@ -314,7 +347,15 @@ void coalesce(void *ptr)
  */
 void mm_free(void *ptr)
 {
-    coalesce(ptr);   
+    coalesce(ptr);
+
+    //printf("free %p\n", ptr);
+    /*
+    printf("after free is :\n");
+    print_list();
+    print_heap();
+    */
+       
 }
 
 /*
@@ -342,7 +383,48 @@ void *mm_realloc(void *ptr, size_t size)
     mm_free(oldptr);
     return newptr;
 
+}
+
+
+void *mm_realloc_old(void *ptr, size_t size)
+{
+    void *oldptr = ptr;
+    int old_size = GET_SIZE(HDRP(ptr));
     
+    int actual_size = MAX(ALIGN(size) + SIZE_T_SIZE*2, MINIMUM_SIZE);
+    //printf("realloc oldsize is %d and size is %d\n", old_size, size);
+    if (actual_size == old_size) {
+        return ptr;
+    }
+    
+    void *old_header = HDRP(ptr);
+    void *old_footer = FTRP(ptr);
+    
+    if (old_size >= actual_size + MINIMUM_SIZE) {
+        void *new_footer = old_header + actual_size - SIZE_T_SIZE;    
+        PUT(old_header, PACK(actual_size, 1));
+        PUT(new_footer, PACK(actual_size, 1));
+        int delta_size = old_size - actual_size;
+        PUT(old_header+actual_size, PACK(delta_size, 0));
+        PUT(old_footer, PACK(delta_size, 0));
+        // TODO
+        coalesce(ptr + actual_size);
+        return ptr;
+    }
+    if (old_size >= actual_size && old_size < actual_size + MINIMUM_SIZE) {
+        return ptr;
+    }
+    if (old_size < actual_size) {
+        void *newptr = mm_malloc(size);
+        if (newptr == NULL)
+            return NULL;
+        memcpy(newptr, oldptr, size); 
+        mm_free(oldptr);
+        return newptr;
+    }
+
+    return NULL;
+
 }
 
 
